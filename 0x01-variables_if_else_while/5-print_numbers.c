a788aa6a2a67075e520869c0db04c32c1a7c35b3
tivereidoro <tivereidoro@yahoo.com>
2022-09-09 21:21:56 -0700

    code to print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - This is the main function
+ * Description: It prints all single digit numbers followed by new line
+ * Return: the function returns 0
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+	{
+		printf("%d", num);
+	}
+
+	printf("\n");
+	return (0);
+}
