ebe6c577a4d54ca842c0ef26170cc2b079deae4d
tivereidoro <tivereidoro@yahoo.com>
2022-09-09 22:05:10 -0700

    update

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -7,27 +7,20 @@
 int main(void)
 {
 	int a = '0';
-
 	int b;
-
 	int c;
 
 	while (a <= '7')
 	{
 		b = a + 1;
-
 		while (b <= '8')
 		{
 			c = b + 1;
-
 			while (c <= '9')
 			{
 				putchar(a);
-
 				putchar(b);
-
 				putchar(c);
-
 				if (a == '7' && b == '8' && c == '9')
 				{
 					putchar('\n');
@@ -35,18 +28,13 @@ int main(void)
 				else
 				{
 					putchar(',');
-
 					putchar(' ');
 				}
 				c++;
 			}
 			b++;
-
 		}
-
 		a++;
-
 	}
-
 	return (0);
 }
