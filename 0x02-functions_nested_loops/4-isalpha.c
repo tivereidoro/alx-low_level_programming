abf01cd7b159384f5e40b9d037035bad2a4571a5
tivereidoro <tivereidoro@yahoo.com>
2022-09-13 19:38:45 -0700

    update fix

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -7,10 +7,10 @@
  */
 int _isalpha(int c)
 {
-	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-		return (1);
+if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	return (1);
 
-		else
+	else
 
-		return (0);
+	return (0);
 }
