69605dbb04ade835e7da72a3ace13c15cd47fcc0
tivereidoro <tivereidoro@yahoo.com>
2022-09-13 17:04:29 -0700

    new project commit h

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+#include <_putchar.h>
+
+/**
+ * main - prints the string with a new line
+ * Return: the function returns 0
+ */
+int main()
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+
+	return (0);
+}
