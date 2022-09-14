db110427a4ec73e2d9356356ea62fa4fd9ccf67f
tivereidoro <tivereidoro@yahoo.com>
2022-09-13 20:11:01 -0700

    update

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _abs - print absolute int value
+ * @x: integer to be passed
+ * Return: absolute value of integer
+ */
+int _abs(int x)
+{
+	if (x > 0)
+	{
+		x *= 1;
+	}
+	else if (x == 0)
+	{
+		x = 0;
+	}
+	else
+	{
+		x *= -1;
+	}
+}
