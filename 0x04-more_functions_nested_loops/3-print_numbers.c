ead9a17de5f6e4517defc0f5404f2e561c0c4ffe
tivereidoro <tivereidoro@yahoo.com>
2022-09-15 17:11:04 -0700

    print number range

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - print numbers 0 - 9
+ *
+ * @
+ *
+ * Return: 0
+ */
+void print_numbers(void)
+{
+	int i = 0;
+
+	while (i <= 9)
+	_putchar(i);
+	i++;
+
+	_putchar('\n');
+}
